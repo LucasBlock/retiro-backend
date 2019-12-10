2c1e0cd5dca5686823909273e936a2dae79f10d1
Lucas Block Villatore <lucas.blockv@gmail.com>
2019-12-09 23:29:23 -0300

    adicionado rotas

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,7 @@
 Route::get('/', function () {
     return view('welcome');
 });
+Route::resource('acampamentos', 'AcampamentoController');
+// Route::get('/acampamentos', 'AcampamentoController@index');
+// Route::get('/acampamentos/criar', 'AcampamentoController@create');
+// Route::post('/acampamentos', 'AcampamentoController@store');
